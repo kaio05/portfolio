960917f6e720da5f944d394de6b12c570bf62718
kaio05 <98607741+kaio05@users.noreply.github.com>
2023-04-20 19:15:08 -0300

    #contacts finished

--- index.js
+++ index.js
@@ -2,3 +2,7 @@ document.querySelector('.menu-toggle').addEventListener('click', () => {
     const menu = document.querySelector('.menu')
     menu.classList.toggle('hide')
 })
+
+const technologies = document.querySelectorAll('.tech')
+
+function 
