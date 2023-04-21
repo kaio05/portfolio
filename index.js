b7ead99f80b374034bda54f6d60375264b46fcfd
kaio05 <98607741+kaio05@users.noreply.github.com>
2023-04-21 11:29:19 -0300

    animations

--- index.js
+++ index.js
@@ -5,4 +5,21 @@ document.querySelector('.menu-toggle').addEventListener('click', () => {
 
 const technologies = document.querySelectorAll('.tech')
 
-function 
+function checkIfOnScreen() {
+    technologies.forEach(function (tech) {
+        console.log(tech.getBoundingClientRect().top)
+        if (tech.getBoundingClientRect().top < 550 && tech.getBoundingClientRect().top > -258) {
+            // console.log('in')
+            tech.classList.add('slideIn')
+            tech.classList.remove('slideOut')
+        } else {
+            // console.log('out')
+            tech.classList.remove('slideIn')
+            tech.classList.add('slideOut')
+        }
+    })
+    console.log('-------')
+}
+
+
+window.onscroll = function() {checkIfOnScreen()}
